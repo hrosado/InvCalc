fda033c7cb9831f10911b6c8e67fdef027b8ad8c
hommy.rosado <hommy.rosado@gmail.com>
2020-04-28 22:24:10 -0400

    Initial solution setup.

---
+++ Controllers/PAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using InvCalc.Models;
+
+namespace InvCalc.Controllers
+{
+    public class PAPIController : ApiController
+    {
+        private InvCalcEntities db = new InvCalcEntities();
+
+        // GET: api/PAPI
+        public IQueryable<PrimaryTable> GetPrimaryTables()
+        {
+            return db.PrimaryTables;
+        }
+
+        // GET: api/PAPI/5
+        [ResponseType(typeof(PrimaryTable))]
+        public async Task<IHttpActionResult> GetPrimaryTable(int id)
+        {
+            PrimaryTable primaryTable = await db.PrimaryTables.FindAsync(id);
+            if (primaryTable == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(primaryTable);
+        }
+
+        // PUT: api/PAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPrimaryTable(int id, PrimaryTable primaryTable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != primaryTable.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(primaryTable).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PrimaryTableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PAPI
+        [ResponseType(typeof(PrimaryTable))]
+        public async Task<IHttpActionResult> PostPrimaryTable(PrimaryTable primaryTable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PrimaryTables.Add(primaryTable);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = primaryTable.ID }, primaryTable);
+        }
+
+        // DELETE: api/PAPI/5
+        [ResponseType(typeof(PrimaryTable))]
+        public async Task<IHttpActionResult> DeletePrimaryTable(int id)
+        {
+            PrimaryTable primaryTable = await db.PrimaryTables.FindAsync(id);
+            if (primaryTable == null)
+            {
+                return NotFound();
+            }
+
+            db.PrimaryTables.Remove(primaryTable);
+            await db.SaveChangesAsync();
+
+            return Ok(primaryTable);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PrimaryTableExists(int id)
+        {
+            return db.PrimaryTables.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ Controllers/PrimaryController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InvCalc.Models;
+
+namespace InvCalc.Controllers
+{
+    public class PrimaryController : Controller
+    {
+        private InvCalcEntities db = new InvCalcEntities();
+
+        // GET: Primary
+        public ActionResult Index()
+        {
+            return View(db.PrimaryTables.ToList());
+        }
+
+        // GET: Primary/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrimaryTable primaryTable = db.PrimaryTables.Find(id);
+            if (primaryTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(primaryTable);
+        }
+
+        // GET: Primary/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Primary/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Date,InitialInvestment,TickerSymbol,PurchasePrice,PurchaseShares,PercentageDesired,TargetSalesPrice,InvestmentYield,EndBalance,Action,Result")] PrimaryTable primaryTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PrimaryTables.Add(primaryTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(primaryTable);
+        }
+
+        // GET: Primary/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrimaryTable primaryTable = db.PrimaryTables.Find(id);
+            if (primaryTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(primaryTable);
+        }
+
+        // POST: Primary/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Date,InitialInvestment,TickerSymbol,PurchasePrice,PurchaseShares,PercentageDesired,TargetSalesPrice,InvestmentYield,EndBalance,Action,Result")] PrimaryTable primaryTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(primaryTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(primaryTable);
+        }
+
+        // GET: Primary/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrimaryTable primaryTable = db.PrimaryTables.Find(id);
+            if (primaryTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(primaryTable);
+        }
+
+        // POST: Primary/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PrimaryTable primaryTable = db.PrimaryTables.Find(id);
+            db.PrimaryTables.Remove(primaryTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
